4c44c400d1841fc55715a03cfefd5ab2d5ff174a
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-02-23 18:18:58 -0600

    components have been imported to App,js and is currently being displayed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './Components/Header';
+import AppBody from './Components/AppBody';
+import Sidebar from './Components/Sidebar';
+import Feed from './Components/Feed';
+import Widgets from './Components/Widgets';
 
 function App() {
   return (
@@ -8,14 +13,20 @@ function App() {
       <h1>Hello World</h1>
 
       {/* HEADER  */}
-      
+      <Header />
 
 
 
       {/* APP BODY */}
+      <AppBody />
       {/* SIDEBAR */}
+      <Sidebar />
       {/* FEED */}
+      <Feed />
       {/* WIDGETS */}
+      <Widgets />
+
+      
     </div>
   );
 }
